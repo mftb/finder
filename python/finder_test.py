18ddaae7040239c1ecb6fb1ae5e8f0e532dccf4a
Matheus Boy <matheus.boy@i-value.com.br>
2020-09-03 19:15:27 -0300

    organizing the test suite

--- python/finder_test.py
+++ python/finder_test.py
@@ -4,23 +4,34 @@ from pytest import raises
 
 
 class FinderTests(unittest.TestCase):
-    ############################
-    # ### setup and teardown ###
-    ############################
-
-    # executed prior to each test
-
-    def setUp(self):
-        pass
-
-    # executed after each test
-    def tearDown(self):
-        pass
-
+    # basic tests
     def test_sanity_check(self):
         string_list = ['asd', 'asdd', 'fre', 'glk', 'lkm']
         finder = Finder(string_list)
         assert finder.find('sad') == ['asd']
+
+    # large tests
+
+    def test_large_constructor_list(self):
+        large_list = ['abc' for i in range(0,1000000)]
+        finder = Finder(large_list)
+        assert finder.find('abc') == large_list
+
+    def test_large_haystack_single_needle(self):
+        large_list = [str(i) for i in range(0,1000000)]
+        finder = Finder(large_list)
+        assert finder.find('123') == ['123', '132', '213', '231', '312', '321']
+
+    # corner case tests
+
+    def test_not_found(self):
+        large_list = [str(i) for i in range(0,1000000)]
+        finder = Finder(large_list)
+        assert finder.find('a') == []
+
+    def test_empty_constructor(self):
+        finder = Finder([])
+        assert finder.find('a') == []
 
     def test_bad_contructor_type(self):
         string_dict = {"asd":"asd", "asdd":"asdd"}
@@ -39,22 +50,3 @@ class FinderTests(unittest.TestCase):
         with raises(TypeError):
             finder = Finder(string_list)
             assert finder.find('asd') == ['asd']
-
-    def test_large_constructor_list(self):
-        large_list = ['abc' for i in range(0,1000000)]
-        finder = Finder(large_list)
-        assert finder.find('abc') == large_list
-
-    def test_large_haystack_single_needle(self):
-        large_list = [str(i) for i in range(0,1000000)]
-        finder = Finder(large_list)
-        assert finder.find('123') == ['123', '132', '213', '231', '312', '321']
-
-    def test_not_found(self):
-        large_list = [str(i) for i in range(0,1000000)]
-        finder = Finder(large_list)
-        assert finder.find('a') == []
-
-    def test_empty_constructor(self):
-        finder = Finder([])
-        assert finder.find('a') == []
