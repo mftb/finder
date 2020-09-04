785568cb93cd601b32dd40a3d1c192e162e9b65e
Matheus Boy <matheus.boy@i-value.com.br>
2020-09-04 12:29:47 -0300

    optimizing finder

--- python/finder.py
+++ python/finder.py
@@ -6,7 +6,12 @@ class Finder():
 
         # lets build a list of tuples containing both the string
         # and its char frequency dictionary for each string in haystack
-        self._haystack = [Finder._string2dict(x) for x in haystack]
+        self._haystack = {}
+        for x in haystack:
+            if len(x) not in self._haystack:
+                self._haystack[len(x)] = [Finder._string2dict(x)]
+            else:
+                self._haystack[len(x)].append(Finder._string2dict(x))
 
     # returns a tuple with the input string and its char frequency dict
     # runs in O(N)
@@ -30,8 +35,10 @@ class Finder():
             raise TypeError
 
         needle, needle_dict = Finder._string2dict(needle)
+        needle_size = len(needle)
         found = []
-        for string, adict in self._haystack:
-            if needle_dict == adict:
-                found.append(string)
+        if needle_size in self._haystack:
+            for string, adict in self._haystack[needle_size]:
+                if needle_dict == adict:
+                    found.append(string)
         return found
